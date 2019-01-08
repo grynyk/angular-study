bed6670eb78c45d7e79123339e65a1990d3d781a
Danylo Grynyk <dgrynyk@ec.localhost>
2019-01-08 15:30:31 +0100

    getting data from store and displaying it on main page

--- ngRx/src/app/app.component.ts
+++ ngRx/src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Cars, Car } from './models/car.model';
+import { Store } from '@ngrx/store';
+import { AppState } from './redux/app.state';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
  public cars: Car[] = [];
+public carState: Observable<Cars>
+ constructor(private store: Store<AppState>){
+
+ }
+
+ ngOnInit(){
+  //  this.store.select('carPage').subscribe(({cars}) =>{
+  //    this.cars = cars;
+  //  });
+
+  this.carState = this.store.select('carPage');
+ }
 
  onAdd(car: Car){
    this.cars.push(car);
--- ngRx/src/app/app.module.ts
+++ ngRx/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { CarsFormComponent } from './cars-form/cars-form.component';
 import { CarComponent } from './car/car.component';
-import { FormsModule } from '@angular/forms';
+import { carsReducer } from './redux/cars.reducer';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    StoreModule.forRoot({carPage: carsReducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
