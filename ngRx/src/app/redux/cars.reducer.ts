eec10a8da35ec7abde0157da8660c970676ce72c
Danylo Grynyk <dgrynyk@ec.localhost>
2019-01-08 14:52:41 +0100

    created reducer connected with action ADD_CAR

--- ngRx/src/app/redux/cars.reducer.ts
+++ ngRx/src/app/redux/cars.reducer.ts
@@ -1,3 +1,24 @@
-export function carsReducer(state = {}, action){
+import { Action } from "@ngrx/store";
+import { Car } from "../models/car.model";
+import { CAR_ACTION, AddCar } from "./cars.action";
 
+const initialState = {
+    cars:[
+        new Car('Ford','12.12.12','Focus',false,1),
+        new Car('Opel','12.10.10','Corsa',false,2),
+        new Car('Audi','12.10.11','A4',false,3),
+    ]
+}
+
+export function carsReducer(state = initialState, action: AddCar){
+    switch(action.type){
+        case CAR_ACTION.ADD_CAR:
+        return {
+            ...state,
+            cars:[...state.cars, action.payload]
+        }
+        default:
+            return state
+
+    }
 }
